772959aa8628ce4c8cec58f49768a918f706e744
DessertWarrior <gly310@gmail.com>
2023-12-28 19:30:31 -0800

    maxProfit solution

--- bestBuyNSell.ts
+++ bestBuyNSell.ts
@@ -1,17 +1,21 @@
 function maxProfit(prices: number[]): number {
     /* Time complexity: O(n) */
-    /* Completion Time: 18:46.73 */
+    /* Completion Time: 19.59.39 */
     // Psudocode:
     // 1. loop thru the prices array. Starting from 1
-    // 2. if target is larger than current. store current to target
-    // 3. if target is smaller than current. Calculate total, if larger than profit, replace.
-    let profit: number = 0;
+    // 2. if target is larger than current. 
+        // 2.1. set target to current.
+        // 2.2. Add selling profit to total profit.
+        // 2.3. selling profit return to 0
+    // 3. if target is smaller than current. Add profit to total
+    let totalProfit: number = 0;
     let target: number = prices[0];
     for (let i: number = 1; i < prices.length; i++) {
-        if (target >= prices[i]) target = prices[i];
-        else (prices[i]-target > profit)? profit = prices[i]-target:null;
+        if (target < prices[i]) 
+            totalProfit += prices[i]- target;
+        target = prices[i];
     }
-    return profit;
+    return totalProfit;
 };
 
 export {maxProfit}

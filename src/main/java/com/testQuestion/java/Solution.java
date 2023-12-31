face52ce0f3d002ba72b81cb6e7a8286d82f0eaa
DessertWarrior <gly310@gmail.com>
2023-12-30 23:29:19 -0800

    canCompleteCircuit solution

--- src/main/java/com/testQuestion/java/Solution.java
+++ src/main/java/com/testQuestion/java/Solution.java
@@ -4,49 +4,49 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 class Solution {
-    public static int[] productExceptSelf(int[] nums) {
-        // initial concept using brute force
-        /* Time complexity: O(n^2) */
-        /* Completion Time: 5.22.36 */
-        int[] arr = new int[nums.length];
-        for (int i=0; i< nums.length; i++) {
-            int result = 1;
-            for (int j=0; j< nums.length; j++) {
-                if (i != j) {
-                    result *=nums[j];
-                }
-
+    public static int canCompleteCircuit(int[] gas, int[] cost) {
+        // Solution using brute force
+        /* Time Complexity: O(n^2) */
+        /* Completion Time: 21:02.35 */
+        for (int i = 0; i < gas.length; i++) {
+            int tGas = 0;
+            for (int j = 0; j < cost.length; j++) {               
+                int current = (i + j) % gas.length;
+                tGas += gas[current];
+                tGas -= cost[current];
+                if (tGas < 0)
+                    break; // if gas reach 0 before arrival
+                
             }
-            arr[i] = result;
+            if (tGas >= 0)
+                return i;
         }
-        return arr;
-        
+        return -1;
     }
-    public static int[] productExceptSelf1(int[] nums) {
-        // solution using O(n)
-        // procedure.
-        // we substitude by left side first. without calculating the self. (save edges)
-        // it can be accomplished if we assign a left variable
-        // 1. we get to index 0, because theres no value, we multiply itself by 1.
-        // 2. then it comes to index 1, we multiply left with value of index 0.
-        // 3. continue the process 
-        // 4. once we reached end, store the value of last to the right variable
-        // 5. since we have original nums array, we can just multiply by rightside of last to first
-        // Visual demonstration
-        /*
-         * 1 num[0] num[0]*num[1] num[0]*num[1]*num[2]
-         * num[1]*nums[2]*nums[3] num[0]*nums[2]*nums[3] num[0]*num[1]*num[3] num[0]*num[1]*num[2]
-         */
-        int [] arr = new int[nums.length];
-        arr[0] = 1;
-        for (int i = 1; i < nums.length; i++) {
-            arr[i] = arr[i-1] *nums[i-1];
-        } //we added all the elements thats to their left
-        int right = 1;
-        for (int i = nums.length-1;i >= 0; i--) {
-            arr[i] *= right;
-            right *= nums[i];
+    public static int canCompleteCircuit1(int[] gas, int[] cost) {
+        /* Time Complexity: O(n) */
+        // check its possible to reach destination
+        int total = 0;
+        for (int i = 0; i< cost.length; i++) 
+            total += gas[i] - cost[i];
+        // if gas is not enough to finish circuit.    
+        if (total < 0) 
+            return -1;
+        //because there's going to be a unique solution
+        // we will go thru entire array to do trails of error. 
+        // Either we find it in the middle and continue to the end, or last is the one
+        int cGas = 0;
+        int current = 0;
+        for (int i = 0; i < cost.length; i++) {
+            cGas += gas[i] - cost[i];
+            // condition doesnt match, check next i.
+            if (cGas < 0) {
+                //if we can reach to i, then there's no way current doesn't work but i does
+                //But if cGas is below, then we need to look for another current
+                current = i + 1;
+                cGas = 0;
+            }
         }
-        return arr;
+        return current;
     }
 }

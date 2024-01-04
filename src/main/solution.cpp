e0d75430b6645734e9656830c35af14bafcfacf4
DessertWarrior <gly310@gmail.com>
2024-01-03 22:38:05 -0800

    solution added

--- src/main/solution.cpp
+++ src/main/solution.cpp
@@ -1,64 +1,17 @@
 #include <string>
-#include <cmath>
+#include <algorithm>
 using namespace std;
+
 class Solution {
 public:
-    string intToRoman(int num) {
-        string symbols[] = {"I","V","X","L","C","D","M"};
-        string result = "";
-
-        for (int i = 0; i < log10(num)+1; i++) {
-            string temp = "";
-            int remainder = int(num/pow(10,i))%10;
-            switch (remainder) {
-                case 4:
-                    temp = symbols[2 * i] + symbols[2 * i+1]; 
-                    break;
-                case 9:
-                    temp = symbols[2 * i] + symbols[2 * (i+1)];
-                    break;
-                default:
-                    if (remainder >= 5) {
-                        remainder -= 5;
-                        temp += symbols[2 * i+1];
-                    }
-                    while (remainder!= 0) {
-                        remainder -= 1;
-                        temp += symbols[2 * i];
-                    }
-            }
-            result = temp + result;
-
+    int lengthOfLastWord(string s) {
+        int count = 0;
+        for (int i = s.length()-1; i >= 0; i--) {
+            if (s[i] != ' ') 
+                count ++;
+            else if  (s[i] == ' ' && count != 0)
+                break;
         }
-        return result;
-        // base condition: s = 0
-    //     if (num == 0) return "";
-    //     //if num is bigger or equal than 1000
-    //     if (num >= 1000)
-    //         return "M" + intToRoman(num-1000);
-    //     else if (num >= 900)
-    //         return "CM" + intToRoman(num-900);
-    //     else if (num >= 500) 
-    //         return "D" + intToRoman(num-500);
-    //     else if (num >= 400) 
-    //         return "CD" + intToRoman(num-400);
-    //     else if (num >= 100)
-    //         return "C" + intToRoman(num- 100);
-    //     else if (num >= 90)
-    //         return "XC" + intToRoman(num-90);
-    //     else if (num >= 50)
-    //         return "L" + intToRoman(num-50);
-    //     else if (num >= 40) 
-    //         return "XL" + intToRoman(num-40);
-    //     else if (num >= 10)
-    //         return "X" + intToRoman(num-10);
-    //     else if (num >= 9)
-    //         return "IX" + intToRoman(num-9);
-    //     else if (num >= 5)
-    //         return "V" + intToRoman(num-5);
-    //     else if (num >=4)
-    //         return "IV" + intToRoman(num-4);
-    //     else   
-    //         return "I" + intToRoman(num-1);
+        return count;
     }
 };
--- src/test/test.cpp
+++ src/test/test.cpp
@@ -1,53 +1,22 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 #include "../main/solution.cpp"
-#include <cstdint>
 
 TEST_CASE("All numbers are converted to roman numerals","[Solution]") {
     Solution s;
 
-    REQUIRE( s.intToRoman(1) == "I" );
-    REQUIRE( s.intToRoman(2) == "II" );
-    REQUIRE( s.intToRoman(3) == "III" );
-    REQUIRE( s.intToRoman(4) == "IV" );
-    REQUIRE( s.intToRoman(5) == "V" );
-    REQUIRE( s.intToRoman(6) == "VI" );
-    REQUIRE( s.intToRoman(9) == "IX" );
-    REQUIRE( s.intToRoman(10) == "X" );
-    REQUIRE( s.intToRoman(11) == "XI" );
-    REQUIRE( s.intToRoman(15) == "XV" );
-    REQUIRE( s.intToRoman(16) == "XVI" );
-    REQUIRE( s.intToRoman(19) == "XIX" );
-    REQUIRE( s.intToRoman(20) == "XX" );
-    REQUIRE( s.intToRoman(21) == "XXI" );
-    REQUIRE( s.intToRoman(25) == "XXV" );
-    REQUIRE( s.intToRoman(26) == "XXVI" );
-    REQUIRE( s.intToRoman(29) == "XXIX" );
-    REQUIRE( s.intToRoman(30) == "XXX" );
-    REQUIRE( s.intToRoman(40) == "XL" );
-    REQUIRE( s.intToRoman(45) == "XLV" );
-    REQUIRE( s.intToRoman(50) == "L" );
-    REQUIRE( s.intToRoman(60) == "LX" );
-    REQUIRE( s.intToRoman(70) == "LXX" );
-    REQUIRE( s.intToRoman(80) == "LXXX" );
-    REQUIRE( s.intToRoman(90) == "XC" );
-    REQUIRE( s.intToRoman(99) == "XCIX" );
-    REQUIRE( s.intToRoman(100) == "C" );
-    REQUIRE( s.intToRoman(200) == "CC" );
-    REQUIRE( s.intToRoman(300) == "CCC" );
-    REQUIRE( s.intToRoman(400) == "CD" );
-    REQUIRE( s.intToRoman(500) == "D" );
-    REQUIRE( s.intToRoman(600) == "DC" );
-    REQUIRE( s.intToRoman(700) == "DCC" );
-    REQUIRE( s.intToRoman(800) == "DCCC" );
-    REQUIRE( s.intToRoman(900) == "CM" );
-    REQUIRE( s.intToRoman(1000) == "M" );
-    REQUIRE( s.intToRoman(1499) == "MCDXCIX" );
-    REQUIRE( s.intToRoman(1500) == "MD" );
-    REQUIRE( s.intToRoman(1800) == "MDCCC" );
-    REQUIRE( s.intToRoman(2000) == "MM" );
-    REQUIRE( s.intToRoman(3000) == "MMM" );
-    REQUIRE( s.intToRoman(3999) == "MMMCMXCIX" );
+    REQUIRE( s.lengthOfLastWord("Hello World") == 5 );
+    REQUIRE( s.lengthOfLastWord("Hello") == 5 );
+    REQUIRE( s.lengthOfLastWord("Hello ") == 5 );
+    REQUIRE( s.lengthOfLastWord(" van s goah ") == 4 );
+    REQUIRE( s.lengthOfLastWord("g") == 1 );
+    REQUIRE( s.lengthOfLastWord("luffy is still joyboy") == 6 );
+    REQUIRE( s.lengthOfLastWord("   fly me   to   the moon  ") == 4 );
+    REQUIRE( s.lengthOfLastWord(" day ") == 3 );
+    REQUIRE( s.lengthOfLastWord("it is a nice day today") == 5 );
+    REQUIRE( s.lengthOfLastWord("   strong    cat                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                ") == 3 );
+    REQUIRE( s.lengthOfLastWord("abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyzabc") == 29 );
+   
 
     
 }

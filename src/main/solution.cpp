a21600f1307b3e808fd2f611f60677d87a86aa70
DessertWarrior <gly310@gmail.com>
2024-01-06 23:01:50 -0800

    solution added

--- src/main/solution.cpp
+++ src/main/solution.cpp
@@ -4,29 +4,71 @@
 #include <vector>
 #include <map>
 using namespace std;
-class Solution {
+/*
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+class Solution
+{
 public:
-    bool isValid(string s) {
-        stack<char> stack;
-        stack.push(s[0]);   //first character
+    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
+    {
+        ListNode *head = l1;
+        ListNode *prev = l1;
+        // store l1 into the stack,
+        int addup = 0;
+        while (l1 != nullptr && l2 != nullptr)
+        {
+            int digit = l1->val + l2->val + addup;
+            addup = digit / 10;
+            l1->val = digit % 10;
 
-        for (int i = 1; i < s.length(); i ++) {
-            
-            if (stack.empty())
-                stack.push(s[i]);
-            else if (stack.top() == '(' && s[i] == ')')
-                stack.pop();
-            else if(stack.top() == '[' && s[i] == ']')
-                stack.pop();
-            else if (stack.top() == '{' && s[i] == '}')
-                stack.pop();
-            else
-                stack.push(s[i]);
+            prev = l1; // last valid l1
+            l1 = l1->next;
+            l2 = l2->next;
+        }
+        // theres a chance of 1 addup plus digit 9
+        while (l1 != nullptr)
+        {
+            int digit = l1->val + addup;
+            addup = digit / 10;
+            l1->val = digit % 10;
+            prev = l1;
+            l1 = l1->next;
         }
 
-        //once operation is complete and theres any remainder
-        if (!stack.empty()) 
-            return false;
-        return true;
+        // since l1 become nullptr. we need to move current l2 to l1
+        if (l2 != nullptr)
+        {
+            prev->next = l2;
+
+            while (prev->next != nullptr)
+            {
+                prev = prev->next;
+                int digit = prev->val + addup;
+                addup = digit / 10;
+                prev->val = digit % 10; 
+            }
+        }
+
+        //if there is still addup, we need to add a new node
+        if (addup == 1) 
+            prev->next = new ListNode(1);
+
+        return head;
     }
 };
--- src/test/test.cpp
+++ src/test/test.cpp
@@ -6,42 +6,55 @@
 TEST_CASE("Test case 1", "[nextGreaterElement]")
 {
     Solution s;
-    string str = "()";
     
-    REQUIRE( s.isValid(str) );
+    ListNode* l1 = new ListNode(2);
+    l1->next = new ListNode(4);
+    l1->next->next = new ListNode(3);
+
+    ListNode* l2 = new ListNode(5);
+    l2->next = new ListNode(6);
+    l2->next->next = new ListNode(4);
+
+    ListNode* ans = s.addTwoNumbers(l1,l2);
+    REQUIRE(ans->val == 7);
+    REQUIRE(ans->next->val == 0);
+    REQUIRE(ans->next->next->val == 8);
 }
 TEST_CASE("Test case 2", "[nextGreaterElement]")
 {
     Solution s;
-    string str = "()[]{}";
     
-    REQUIRE( s.isValid(str) );
+    ListNode* l1 = new ListNode(0);
+
+    ListNode* l2 = new ListNode(0);
+
+    ListNode* ans = s.addTwoNumbers(l1,l2);
+    REQUIRE(ans->val == 0);
 }
 TEST_CASE("Test case 3", "[nextGreaterElement]")
 {
     Solution s;
-    string str = "(]";
     
-    REQUIRE( !s.isValid(str) );
-}
-TEST_CASE("Test case 4", "[nextGreaterElement]")
-{
-    Solution s;
-    string str = "([)]";
-    
-    REQUIRE( !s.isValid(str) );
-}
-TEST_CASE("Test case 5", "[nextGreaterElement]")
-{
-    Solution s;
-    string str = "{[]}";
-    
-    REQUIRE( s.isValid(str) );
-}
-TEST_CASE("Test case 6", "[nextGreaterElement]")
-{
-    Solution s;
-    string str = "([}}])";
-    
-    REQUIRE( !s.isValid(str) );
+    ListNode* l1 = new ListNode(9);
+    l1->next = new ListNode(9);
+    l1->next->next = new ListNode(9);
+    l1->next->next->next = new ListNode(9);
+    l1->next->next->next->next = new ListNode(9);
+    l1->next->next->next->next->next = new ListNode(9);
+    l1->next->next->next->next->next->next = new ListNode(9);
+
+    ListNode* l2 = new ListNode(9);
+    l2->next = new ListNode(9);
+    l2->next->next = new ListNode(9);
+    l2->next->next->next = new ListNode(9);
+
+    ListNode* ans = s.addTwoNumbers(l1,l2);
+    REQUIRE(ans->val == 8);
+    REQUIRE(ans->next->val == 9);
+    REQUIRE(ans->next->next->val == 9);
+    REQUIRE(ans->next->next->next->val == 9);
+    REQUIRE(ans->next->next->next->next->val == 0);
+    REQUIRE(ans->next->next->next->next->next->val == 0);
+    REQUIRE(ans->next->next->next->next->next->next->val == 0);
+    REQUIRE(ans->next->next->next->next->next->next->next->val == 1);
 }
